e20a7229855c85340ae9eef3d51713b7cfbbe5ec
Robert Shields <shieldrj@mail.uc.edu>
2017-04-12 21:52:23 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,10 @@
+class ControllerMailer < ActionMailer::Base
+  default to: 'shieldrj@mail.uc.edu'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
